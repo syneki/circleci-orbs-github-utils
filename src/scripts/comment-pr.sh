8bb6e1be91c15232a280fab998b80d6f8bd49c09
Martin PAUCOT <m.paucot@kmp.agency>
2022-10-12 17:20:59 +0200

    feat: add multiple request feature

--- src/scripts/comment-pr.sh
+++ src/scripts/comment-pr.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-IFS=',' read -r -a array <<< "$string"
+PULL_REQUESTS=$(eval "echo \"\$${PARAM_PR}\"")
+
+IFS=',' read -r -a array <<< "${PULL_REQUESTS}"
 for element in "${array[@]}"
 do
     eval "gh pr comment \"${element}\" -b \"${PARAM_BODY}\""
